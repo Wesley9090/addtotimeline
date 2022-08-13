9bde725202bab67b1b1ebd66a754af70bceb5bf8
Wesley9090 <88640690+Wesley9090@users.noreply.github.com>
2022-08-12 23:20:46 -0400

    Update form-add.js

--- form-add.js
+++ form-add.js
@@ -1,38 +1,39 @@
 
-
-  
-var eName = (formAdd.name.value)
-
-
-    var eDate = (formAdd.date.value)
-    var eDescription = (formAdd.description.value)
-
-var btnSalvar = document.querySelector("#save");
-
-btnSalvar.addEventListener("click", function (event) {
-    event.preventDefault();
-
+function Abrir(id) {
+    document.getElementById(id).style.display = "block";
+    Inserir()
     var formAdd = document.querySelector("#formAdd");
-
 
     let name = (formAdd.name.value)
     let date = (formAdd.date.value)
     let description = (formAdd.description.value)
 
-
-
-
-
-
-    console.log(`Nome: `, name)
-    console.log(formAdd.name.value);
-    console.log(formAdd.date.value);
-    console.log(formAdd.description.value);
     localStorage.setItem('name', name);
     localStorage.setItem('date', date);
     localStorage.setItem('description', description);
 
-    
+    document.querySelector("#nameInner").innerHTML = name;
+    document.querySelector("#dateInner").innerHTML = date;
+    document.querySelector("#descriptionInner").innerHTML = description;
+}
 
+function edit(id) {
+    document.getElementById(id).style.display = "none";
+}
+function save() {
+
+    window.location.href = './confirm.html'
+}
+function Inserir() {
+    let NameP = document.querySelector('#Name');
+    let dateP = document.querySelector('#Date');
+    let descriptionP = document.querySelector('#Description');
+    //console.log(dadosDiv);
+}
+var eDate = (formAdd.date.value)
+var eDescription = (formAdd.description.value)
+var btnSalvar = document.querySelector("#save");
+btnSalvar.addEventListener("click", function (event) {
+    event.preventDefault();
 })
 
